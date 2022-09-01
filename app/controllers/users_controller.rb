b0f43bdf9272d662cd911865e95c11f868ab43bf
Muzammalabbas59 <abmuzammal17@gmail.com>
2022-09-01 21:11:51 +0500

    add user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,19 @@
 class UsersController < ApplicationController
-    before_action :set_user , only: [ :edit,:update]
+    before_action :set_user , only: [ :edit,:update,:show]
 
     def new
         @user=User.new
     end 
 
     def edit
+
     end
 
+    def show 
+
+    end    
+
     def create
-       
        @user=User.new(user_params)
        if @user.save
           flash[:notice]="Dear #{@user.username}, Welcome to alpha blog" 
