8402e0d4ade2d3b04762065e41b077f743b9ecf0
Muzammalabbas59 <abmuzammal17@gmail.com>
2022-09-05 19:02:47 +0500

    Restrict Navigation actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,54 +1,52 @@
 class ArticlesController < ApplicationController
-  before_action :set_article , only: [ :show, :edit, :update, :destroy ]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def show
-     end
+  end
 
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
-
   end
 
   def new
-    @article=Article.new
+    @article = Article.new
   end
+
   def edit
-    end
+  end
 
   def create
-    
-    @article=Article.new(article_params)
-    @article.user=User.first
+    @article = Article.new(article_params)
+    @article.user = current_user
     if @article.save
-      flash[:notice]="created successfully"
-    redirect_to (@article)
+      flash[:notice] = "created successfully"
+      redirect_to (@article)
     else
-      render 'new'
-    end  
-  end 
-  
-  def update 
-    if @article.update(article_params)
-    flash[:notice]="updated successfully"
-    redirect_to (@article)
-    else
-      render 'edit'
+      render "new"
     end
   end
 
-  def destroy 
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "updated successfully"
+      redirect_to (@article)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
     @article.destroy
     redirect_to articles_path, status: :see_other
   end
 
-  private 
-    def set_article
-    @article = Article.find(params[:id])  
-    end
+  private
 
-    def article_params
-    params.require(:article).permit(:title,:description)
-    end
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
-
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,22 @@
 class SessionsController < ApplicationController
-def new
+  def new
+  end
 
-end
-def create
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "logged in Successfully"
+      redirect_to user
+    else
+      flash.now[:alert] = "Wrong Password or Email"
+      render "new"
+    end
+  end
 
-end
-
-def destroy
-
-end
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "logged Out Successfully"
+    redirect_to login_path, status: :see_other
+  end
 end
