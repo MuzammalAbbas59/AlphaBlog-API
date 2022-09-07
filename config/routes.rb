5b6505a58ba320d41285ae0439dd2086524e26f8
Muzammalabbas59 <abmuzammal17@gmail.com>
2022-09-07 19:21:16 +0500

    Add category test cases

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'about', to:'pages#about'
   get 'signup', to:'users#new'  
   resources :users, except: [:new]
+  resources :categories, except: [:destroy]
 
   get 'login', to:'sessions#new'  
   post 'login', to:'sessions#create'  
