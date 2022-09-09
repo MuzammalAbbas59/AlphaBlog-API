3aee1b40242780ede6a2da68aee23cd56d4dd931
Muzammalabbas59 <abmuzammal17@gmail.com>
2022-09-09 18:35:18 +0500

    refactored final  Code

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,23 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show]
-  before_action :require_admin, except: [:index, :show]
-
+  before_action :set_category, only: [:show,:edit,:update]
+  before_action :require_admin, except: [:index, :show,:edit,:update]
+  before_action :require_user, except: [:show, :index]
+  
   def new
     @category = Category.new
+  end
+   
+  def edit
+
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:notice] = "updated successfully"
+      redirect_to category_path(@category)
+    else
+      render "edit"
+    end
   end
 
   def index
@@ -11,6 +25,7 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @articles = @category.articles.paginate(page: params[:page], per_page:5)
   end
 
   def create
