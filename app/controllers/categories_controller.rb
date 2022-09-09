c2dca7b0580c28a5c08e9d0c70557976c70011da
Muzammalabbas59 <abmuzammal17@gmail.com>
2022-09-09 17:50:48 +0500

    update article views to add categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,42 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show]
+  before_action :require_admin, except: [:index, :show]
+
   def new
+    @category = Category.new
   end
 
   def index
+    @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def show
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "created successfully"
+      redirect_to @category
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def require_admin
+    if !(loggedin? && current_user.admin?)
+      flash[:alert] = "Only Admin can perform this action"
+      redirect_to categories_path
+    end
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ApplicationRecord
   validates :name, presence: true,
                    uniqueness: { case_sensitive: false },
                    length: { minimum: 4, maximum: 20 }
+
+  has_many :article_categories
+  has_many :articles, through: :article_categories
 end
